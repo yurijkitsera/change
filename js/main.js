4ce9bf21fcf1ae1448bcfac08c8b0d6671502f15
yurijkitsera <101573698+yurijkitsera@users.noreply.github.com>
2023-02-06 16:34:09 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-let input = document.querySelector('input');
-console.log(input.value);
+$(document).ready(function () {
+    $('#btn-send2').click(function () {
+        var ref = $('#ref').val();
+        if (ref !== '') {
+            $('span').removeClass('hidden')
+        }
+        if (ref !== '') {
+            $.ajax({
+                type: "POST", //метод передачи данных
+                url: 'https://0-570-0.app.nr.it.loc/change',
+                data: {
+                    ref: ref,
+                }, //передаваемые данные
+                success: function (data) { //получение результата
+                    $("span").addClass("hidden");
+                    var old = $('.modal-header').html(); //получаем содежимое div
+                    $('.modal-header').html(old +
+                        data.status); //добавляем сообщение об отправке
+                }
+            });
+        }
+
+    });
+});
